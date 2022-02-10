234c409edbfd2416ab97bc7f184b3f9f271f784d
BobLovesData <bob@MassStreet.net>
2022-02-09 23:47:10 -0600

    Oops that's gotta go too!

--- Example Code/FIle Rename Test.py
+++
@@ -1,7 +0,0 @@
-file_name = 'Q23.jpg'
-
-index = file_name.find('.')
-renamed_file = file_name[:index] + '01' + file_name[index:]
-renamed_file = renamed_file.replace('Q', '')
-
-print(renamed_file)
--- Example Code/testconnection.py
+++
@@ -1,7 +0,0 @@
-import pyodbc
-import pandas as pd
-print('test')
-conn = pyodbc.connect(driver='{SQL Server}', server='ALPHA', trusted_connection='yes')
-print('test')
-
-#cursor = cnxn.cursor()
